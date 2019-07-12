8ac8fe3f70d4223b12c148b77586504ff0588da0
abaron <aryobaron@gmail.com>
2019-07-12 21:37:11 +0700

    sitemap

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
 if (isset($_GET['page']) && file_exists(dirname(__FILE__) . DIRECTORY_SEPARATOR . "npl" . DIRECTORY_SEPARATOR . $_GET['page'] . ".php"))
     require_once(dirname(__FILE__) . DIRECTORY_SEPARATOR . "npl" . DIRECTORY_SEPARATOR . $_GET['page'] . ".php");
+else
+    listPages();
+
+function listPages() {
+    $scan = array_filter(scandir('./npl'), function($item) {
+        return !is_dir('./npl/' . $item);
+    });
+
+    foreach ($scan as $value) {
+        $filename = substr($value, 0, -4);
+        echo '<a href="/?page='. $filename .'">' . $filename . '</a><br />';
+    }
+}
